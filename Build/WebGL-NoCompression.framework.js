7cb98c90670a24ae822412095090fc1dbbce2697
Ba6ySHark <steven.susorov@gmail.com>
2026-05-02 13:58:55 -0700

    Adding no-compression build with Mesh rendering fix

---
+++ Build/WebGL-NoCompression.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b3379275322ee64f83d9db9ed2a55a90d8393a9936a7a67d18a260c0d18aa0a
+size 432358
---
+++ Build/WebGL-NoCompression.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcb7bb8ed4aacbe85e14f98834a3db001992d4d14202a29d80edeb0629fbacb0
+size 26454
